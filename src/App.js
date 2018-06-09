7cd7fe4128170f28c852c9bc7308678903a41d36
tw0517tw <tw0517tw@gmail.com>
2018-06-09 18:21:57 +0800

    use reactstrap for style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
-import './App.css';
+import {
+  Navbar,
+  NavbarBrand,
+  Container,
+  Row,
+  Col,
+  Label,
+  Input,
+} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand>勞保健保勞退</NavbarBrand>
+        </Navbar>
+        <Container>
+          <Row>
+            <Col md={{ size: 6, offset: 3 }}>
+              <Label htmlFor="投保薪資">投保薪資</Label>
+              <Input id="投保薪資" />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={{ size: 4 }}>
+              <Label for="健保">健保</Label>
+              <Input id="健保" />
+            </Col>
+            <Col md={{ size: 4 }}>
+              <Label for="勞保">勞保</Label>
+              <Input id="勞保" />
+            </Col>
+            <Col md={{ size: 4 }}>
+              <Label for="勞退">勞退</Label>
+              <Input id="勞退" />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
