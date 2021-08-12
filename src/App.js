0c6ae9f3cb9bfaafd6083b2e309e3053c53e1651
tw0517tw <tw0517tw@gmail.com>
2021-08-13 03:19:13 +0800

    use hooks, all function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Navbar, NavbarBrand, Row } from 'reactstrap';
 
 import EmployeeSalaryForm from './components/EmployeeSalaryForm';
@@ -6,87 +6,78 @@ import HealthInsuranceTable from './components/HealthInsuranceTable';
 import LaborInsuranceTable from './components/LaborInsuranceTable';
 import RetirePlanTable from './components/RetirePlanTable';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [salaryInput, setSalaryInput] = useState('');
+  const [isSalaryValid, setIsSalaryValid] = useState(true);
+  const [lastValidSalary, setLastValidSalary] = useState(0);
 
-    this.state = {
-      salaryInput: '',
-      isSalaryValid: true,
-      lastValidSalary: 0,
-    };
-  }
+  const handleSalaryChange = (event) => {
+    const eventValue = event.target.value;
+    const salaryNumber = +eventValue;
 
-  handleSalaryChange = (e) => {
-    const salaryInput = e.target.value;
-    const salaryNumber = +salaryInput;
+    setSalaryInput(eventValue);
 
     if (Number.isNaN(salaryNumber) || !Number.isFinite(salaryNumber)) {
-      this.setState(() => ({ salaryInput, isSalaryValid: false }));
+      setIsSalaryValid(false);
       return;
     }
-    this.setState(() => ({
-      salaryInput,
-      isSalaryValid: true,
-      lastValidSalary: +salaryInput,
-    }));
+
+    setIsSalaryValid(true);
+    setLastValidSalary(salaryNumber);
   };
 
-  render() {
-    const { salaryInput, isSalaryValid, lastValidSalary } = this.state;
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand>勞保健保勞退</NavbarBrand>
-        </Navbar>
-        <Container>
-          <Row>
-            <Col md={{ size: 6, offset: 3 }}>
-              <EmployeeSalaryForm
-                salary={salaryInput}
-                valid={isSalaryValid}
-                updateSalary={this.handleSalaryChange}
-              />
-            </Col>
-          </Row>
-          <Row style={{ marginTop: '20px' }}>
-            <Col md={{ size: 4 }}>
-              <HealthInsuranceTable salary={lastValidSalary} />
-            </Col>
-            <Col md={{ size: 4 }}>
-              <LaborInsuranceTable salary={lastValidSalary} />
-            </Col>
-            <Col md={{ size: 4 }}>
-              <RetirePlanTable salary={lastValidSalary} />
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center" md={{ size: 6, offset: 3 }}>
-              <span>健保相關資料自 107年1月1日 開始適用</span>
-              <br />
-              健保資料來自
-              <a href="https://www.nhi.gov.tw/Content_List.aspx?n=FFE36DCABA22D234&topn=3185A4DF68749BA9">
-                衛福部健保署
-              </a>
-              <br />
-              <span>勞保、勞退相關資料自 107年1月1日 開始適用</span>
-              <br />
-              勞保資料來自
-              <a href="http://www.bli.gov.tw/sub.aspx?a=UA2ZR%2bHjzD4%3d">
-                勞動部勞保局
-              </a>
-              <br />
-              勞退資料來自
-              <a href="http://www.bli.gov.tw/sub.aspx?a=uyDH38mCe%2fM%3d">
-                勞動部勞保局
-              </a>
-              <br />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>勞保健保勞退</NavbarBrand>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col md={{ size: 6, offset: 3 }}>
+            <EmployeeSalaryForm
+              salary={salaryInput}
+              valid={isSalaryValid}
+              updateSalary={handleSalaryChange}
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginTop: '20px' }}>
+          <Col md={{ size: 4 }}>
+            <HealthInsuranceTable salary={lastValidSalary} />
+          </Col>
+          <Col md={{ size: 4 }}>
+            <LaborInsuranceTable salary={lastValidSalary} />
+          </Col>
+          <Col md={{ size: 4 }}>
+            <RetirePlanTable salary={lastValidSalary} />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center" md={{ size: 6, offset: 3 }}>
+            <span>健保相關資料自 107年1月1日 開始適用</span>
+            <br />
+            健保資料來自
+            <a href="https://www.nhi.gov.tw/Content_List.aspx?n=FFE36DCABA22D234&topn=3185A4DF68749BA9">
+              衛福部健保署
+            </a>
+            <br />
+            <span>勞保、勞退相關資料自 107年1月1日 開始適用</span>
+            <br />
+            勞保資料來自
+            <a href="http://www.bli.gov.tw/sub.aspx?a=UA2ZR%2bHjzD4%3d">
+              勞動部勞保局
+            </a>
+            <br />
+            勞退資料來自
+            <a href="http://www.bli.gov.tw/sub.aspx?a=uyDH38mCe%2fM%3d">
+              勞動部勞保局
+            </a>
+            <br />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default App;
--- src/components/HealthInsuranceTable.js
+++ src/components/HealthInsuranceTable.js
@@ -1,85 +1,75 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Input, Table } from 'reactstrap';
 
 import findRank from '../utils/findRank';
 import healthInsuranceFee from '../data/healthInsuranceFee';
 
-class HealthInsuranceTable extends Component {
-  constructor(props) {
-    super(props);
+const HealthInsuranceTable = ({ salary }) => {
+  const [familyCount, setFamilyCount] = useState(0);
 
-    this.state = {
-      familyCount: 0,
-    };
-  }
+  const familyCountSelect = (
+    <Input
+      type="select"
+      onChange={(event) => {
+        setFamilyCount(+event.target.value);
+      }}
+    >
+      <option value={0}>0</option>
+      <option value={1}>1</option>
+      <option value={2}>2</option>
+      <option value={3}>3或以上</option>
+    </Input>
+  );
 
-  onFamilyCountChange = (e) => {
-    const familyCount = +e.target.value;
-    this.setState(() => ({ familyCount }));
-  };
+  const rankedSalary = findRank(salary, healthInsuranceFee.rank);
+  const totalFee = rankedSalary * healthInsuranceFee.rate;
+  const youPay =
+    Math.round(totalFee * healthInsuranceFee.ratio.company.you) *
+    (1 + familyCount);
+  const bossPay = Math.round(
+    totalFee *
+      healthInsuranceFee.ratio.company.boss *
+      (1 + healthInsuranceFee.avgFamilyMember)
+  );
+  const govPay = Math.round(
+    totalFee *
+      healthInsuranceFee.ratio.company.gov *
+      (1 + healthInsuranceFee.avgFamilyMember)
+  );
 
-  render() {
-    const { salary } = this.props;
-    const { familyCount } = this.state;
-
-    const familyCountSelect = (
-      <Input type="select" onChange={this.onFamilyCountChange}>
-        <option value={0}>0</option>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3或以上</option>
-      </Input>
-    );
-
-    const rankedSalary = findRank(salary, healthInsuranceFee.rank);
-    const totalFee = rankedSalary * healthInsuranceFee.rate;
-    const youPay =
-      Math.round(totalFee * healthInsuranceFee.ratio.company.you) *
-      (1 + familyCount);
-    const bossPay = Math.round(
-      totalFee *
-        healthInsuranceFee.ratio.company.boss *
-        (1 + healthInsuranceFee.avgFamilyMember)
-    );
-    const govPay = Math.round(
-      totalFee *
-        healthInsuranceFee.ratio.company.gov *
-        (1 + healthInsuranceFee.avgFamilyMember)
-    );
-    return (
-      <Table>
-        <thead>
-          <tr>
-            <th colSpan="2">健保</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>投保級距</td>
-            <td>{rankedSalary}</td>
-          </tr>
-          <tr>
-            <td>扶養眷屬</td>
-            <td>{familyCountSelect}</td>
-          </tr>
-          <tr>
-            <td>個人負擔</td>
-            <td>{youPay}</td>
-          </tr>
-          <tr>
-            <td>雇主負擔</td>
-            <td>{bossPay}</td>
-          </tr>
-          <tr>
-            <td>政府負擔</td>
-            <td>{govPay}</td>
-          </tr>
-        </tbody>
-      </Table>
-    );
-  }
-}
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th colSpan="2">健保</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>投保級距</td>
+          <td>{rankedSalary}</td>
+        </tr>
+        <tr>
+          <td>扶養眷屬</td>
+          <td>{familyCountSelect}</td>
+        </tr>
+        <tr>
+          <td>個人負擔</td>
+          <td>{youPay}</td>
+        </tr>
+        <tr>
+          <td>雇主負擔</td>
+          <td>{bossPay}</td>
+        </tr>
+        <tr>
+          <td>政府負擔</td>
+          <td>{govPay}</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
 
 HealthInsuranceTable.propTypes = {
   salary: PropTypes.number.isRequired,
